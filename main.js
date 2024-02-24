8ed23503d2dc1b215e876dc4b516e177c604f138
shahdelshamy <134215710+shahdelshamy@users.noreply.github.com>
2024-02-25 00:06:14 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let image = document.querySelector('.image');
+let background = document.querySelector('.container')
+
+function phoneChange(src) {
+    image.src = src;
+}
+
+function changeBackground(color) {
+    background.style.background = color;
+}
+
+
